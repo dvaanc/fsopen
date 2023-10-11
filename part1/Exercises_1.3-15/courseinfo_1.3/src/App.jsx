97021df9d467d07d2a985535232fedaa8a9417c5
Danny <dannyc52521@gmail.com>
2023-10-11 15:22:07 +1100

    add exercise 1.3

---
+++ part1/Exercises_1.3-15/courseinfo_1.3/src/App.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable react/prop-types */
+const Header = (props) => {
+  return (
+    <>
+      <h1>{ props.course }</h1>
+    </>
+  )
+}
+  
+
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={ props.parts[0] } />
+      <Part part={ props.parts[1] } />
+      <Part part={ props.parts[2] } />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises { props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises }</p>
+  )
+}
+
+const Part= (props) => {
+  return (
+  <p>{ props.part.name } { props.part.exercises }</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={ course } />
+      <Content parts={[ part1, part2, part3]} />
+      <Total parts={[ part1, part2, part3]} />
+    </div>
+  )
+}
+
+export default App
