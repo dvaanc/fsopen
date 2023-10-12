5233b62fff8c132595cd47732440f0fd4a7ae928
Danny <dannyc52521@gmail.com>
2023-10-12 16:21:14 +1100

    add anecdote exercises

--- part1/1.11/src/App.jsx
+++ part1/1.11/src/App.jsx
@@ -9,12 +9,14 @@ const Statistics = ({stats}) => {
   if(total > 0) {
     return (
       <table>
-        <StatisticLine text='good' value={stats.good} />
-        <StatisticLine text='neutral' value={stats.neutral} />
-        <StatisticLine text='bad' value={stats.bad}/>
-        <StatisticLine text='total' value={total}/>
-        <StatisticLine text='average' value={average}/>
-        <StatisticLine text='positive' value={positive}/>
+        <tbody>
+          <StatisticLine text='good' value={stats.good} />
+          <StatisticLine text='neutral' value={stats.neutral} />
+          <StatisticLine text='bad' value={stats.bad}/>
+          <StatisticLine text='total' value={total}/>
+          <StatisticLine text='average' value={average}/>
+          <StatisticLine text='positive' value={positive}/>
+        </tbody>
       </table>
     )
   }
